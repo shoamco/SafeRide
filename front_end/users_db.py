117328959d30e7d9d00d425ca9f2972ac5b337ae
tamarapple <tamaree5@gmail.com>
2019-12-30 18:56:00 +0200

    add login, register, user_window

--- front_end/users_db.py
+++ front_end/users_db.py
@@ -1,8 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, flash, redirect
 from flask_mongoengine import MongoEngine
 import mongoengine as me
+import os
+import base64
+
+################ forms ################
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 app.config['FLASK_ENV'] = True  # development
 app.config['FLASK_APP'] = "users_db.py"
@@ -13,18 +21,56 @@ app.config['MONGODB_SETTINGS'] = {
 db = MongoEngine(app)
 
 
+class RegistrationForm(FlaskForm):
+    user_id = IntegerField("Id", validators=[DataRequired()])
+
+    name = StringField('Name',
+                       validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+
+    phone = IntegerField("Phone", validators=[DataRequired()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    user_id = StringField('User Id',
+                          validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
 class User(me.Document):
     user_id = me.IntField(unique=True)
     name = me.StringField()
+    email = me.StringField()
+    password = me.StringField()
+    phone = me.IntField()
+    num_trips = me.IntField()
     positive_points = me.IntField()
     negative_points = me.IntField()
 
 
-"""@app.route('/')
-def hello_world():
-    user = User(user_id=122, num_travels=30, name='tamar')
-    user.save()
-    return 'Hello, World! TeamUP'"""
+@app.route('/user_window/<current_user_id>', methods=['GET', 'POST'])
+def user_window(current_user_id):
+    # if request.method == 'POST':
+    # user_id = request.form.get('current_user_id')
+    user = User.objects(user_id=int(current_user_id))
+    if user:
+        user = user.get(user_id=int(current_user_id))
+        calc_points = user.positive_points - user.negative_points
+        if calc_points > 0:
+            return render_template("user_interface.html", num=calc_points % 10, name=user.name)
+        else:
+            return render_template("user_interface.html", num=0, name=user.name)
 
 
 @app.route('/add_user', methods=['POST', 'GET'])
@@ -32,7 +78,11 @@ def add_user():
     if request.method == 'POST':
         user_id = request.form.get('user_id')
         name = request.form.get('name')
-        user = User(user_id=user_id, name=name, positive_points=0, negative_points=0)
+        email = request.form.get('email')
+        password = request.form.get('password')
+        phone = request.form.get('phone')
+        user = User(user_id=user_id, name=name, email=email, password=password, phone=phone, num_trips=0,
+                    positive_points=0, negative_points=0)
         user.save()
         return f"hello {user_id},  {name} !"
     return "NOT GET OT POST METHOD WERE USED"
@@ -46,6 +96,7 @@ def update_user():
         user = User.objects(user_id=user_id)
         if user:
             user = user.get(user_id=user_id)
+            user.num_trips += 1
             if int(is_positive) == 1:
                 user.positive_points += 1
             else:
@@ -62,5 +113,74 @@ def users_table_show():
     return render_template("index.html", users=User.objects())
 
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.name.data}!', 'success')
+        user_id = form.user_id.data
+        name = form.name.data
+        email = form.email.data
+        phone = form.phone.data
+        password = form.password.data
+        user = User(user_id=user_id, name=name, email=email, password=password, phone=phone, num_trips=0,
+                    positive_points=0, negative_points=0)
+        user.save()
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_id = form.user_id.data
+        user = User.objects(user_id=user_id)
+        if user:
+            user = user.get(user_id=user_id)
+            password = user.password
+            if form.password.data == password:
+                return user_window(user_id)
+                # return redirect(url_for(f'user_window/{user_id}'))
+
+    #     if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+    #         flash('You have been logged in!', 'success')
+    #         return redirect(url_for('users'))
+    #     else:
+    #         flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+# @app.route("/show_image", methods=['GET', 'POST'])
+# def show_image():
+#     if request.method == "POST":
+#
+#         if request.files:
+#             image = request.files["image"]
+#
+#             image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+#
+#             print("Image saved")
+#
+#             return redirect(request.url)
+#
+#     return render_template("upload_image.html")
+
+@app.route("/upload_image", methods=["POST"])
+def upload_image():
+    jsn = request.get_json()
+    img = jsn['img']
+    #cropped = jsn['cropped']
+    img_name = jsn['img_name']
+    img_name += '.jpg'
+    imgdata = base64.b64decode(img)
+    #crpdata = base64.b64decode(cropped)
+    with open(f'images/{img_name}', 'wb') as f:
+        f.write(imgdata)
+        #c.write(crpdata)
+    return "got the image!"
+
+
 if __name__ == '__main__':
     app.run("0.0.0.0")
+    # app.run()
